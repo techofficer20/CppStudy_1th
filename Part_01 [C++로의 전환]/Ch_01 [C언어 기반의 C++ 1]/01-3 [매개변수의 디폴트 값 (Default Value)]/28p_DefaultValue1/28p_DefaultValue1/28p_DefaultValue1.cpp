fe014021bd1c65f386e912772fc25a045c9cb6d4
techofficer20 <48676268+techofficer20@users.noreply.github.com>
2019-07-23 16:27:10 +0900

    매개변수에 설정하는 '디폴트 값'의 의미

---
+++ Part_01 [C++로의 전환]/Ch_01 [C언어 기반의 C++ 1]/01-3 [매개변수의 디폴트 값 (Default Value)]/28p_DefaultValue1/28p_DefaultValue1/28p_DefaultValue1.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+int Adder(int num1 = 1, int num2 = 2)
+{
+	return num1 + num2;
+}
+
+int main(void)
+{
+	std::cout << Adder() << std::endl; // 3 (아무 인자도 없으므로 기본값인 1 + 2 출력)
+	std::cout << Adder(5) << std::endl; // 7 (첫 번째 인자만 설정했으므로 5+ 2 출력) 왼쪽부터 채워져나간다.
+	std::cout << Adder(3, 5) << std::endl; // 8 (모두 설정했으므로 3 + 5 출력)
+	return 0;
+}
