593fb40082612ae9af1888ae15c00f0f329e1b57
techofficer20 <48676268+techofficer20@users.noreply.github.com>
2019-07-23 16:30:11 +0900

    디폴트 값은 함수의 선언 부분에만 표현하면 됩니다.

---
+++ Part_01 [C++로의 전환]/Ch_01 [C언어 기반의 C++ 1]/01-3 [매개변수의 디폴트 값 (Default Value)]/29p_DefaultValue2/29p_DefaultValue2/29p_DefaultValue2.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+int Adder(int num1 = 1, int num2 = 2); // 디폴트 값은 함수의 선언 부분에만 표현하면 된다.
+
+int main(void)
+{
+	std::cout << Adder() << std::endl;
+	std::cout << Adder(5) << std::endl;
+	std::cout << Adder(3, 5) << std::endl;
+	return 0;
+}
+
+int Adder(int num1, int num2)
+{
+	return num1 + num2;
+}
