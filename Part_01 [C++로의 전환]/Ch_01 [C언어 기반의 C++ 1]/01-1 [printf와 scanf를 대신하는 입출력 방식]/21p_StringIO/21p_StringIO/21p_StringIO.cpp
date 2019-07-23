25fae5e1472231b7d72e7eaa4060a6086e84bd2d
techofficer20 <48676268+techofficer20@users.noreply.github.com>
2019-07-23 15:28:09 +0900

    배열 기반의 문자열 입출력

---
+++ Part_01 [C++로의 전환]/Ch_01 [C언어 기반의 C++ 1]/01-1 [printf와 scanf를 대신하는 입출력 방식]/21p_StringIO/21p_StringIO/21p_StringIO.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+int main(void)
+{
+	char name[100];
+	char lang[200];
+
+	std::cout << "이름은 무엇입니까? ";
+	std::cin >> name;
+
+	std::cout << "좋아하는 프로그래밍 언어는 무엇인가요? ";
+	std::cin >> lang;
+
+	std::cout << "내 이름은 " << name << "입니다.\n";
+	// '\n'과 같은 특수문자는 C언어에서와 같은 의미를 지닌다. 다만 std::endl로 개행의 역할을 대신할 뿐이다.
+	std::cout << "제일 좋아하는 언어는 " << lang << "입니다." << std::endl;
+	return 0;
+}
