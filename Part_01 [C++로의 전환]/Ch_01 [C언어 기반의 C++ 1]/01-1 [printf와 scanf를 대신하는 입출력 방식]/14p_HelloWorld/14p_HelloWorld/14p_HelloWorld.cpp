b918770f97781efa3f11238169a2ea828b96c6fa
techofficer20 <48676268+techofficer20@users.noreply.github.com>
2019-07-23 15:19:13 +0900

    문자열 "Hello world"의 출력

---
+++ Part_01 [C++로의 전환]/Ch_01 [C언어 기반의 C++ 1]/01-1 [printf와 scanf를 대신하는 입출력 방식]/14p_HelloWorld/14p_HelloWorld/14p_HelloWorld.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream> // 헤더 파일 선언
+
+int main(void)
+{
+	int num = 20;
+	std::cout << "Hello World!" << std::endl; // endl: endline. c언어에서의 개행
+	std::cout << "Hello " << "World!" << std::endl;
+	std::cout << num << ' ' << 'A';
+	std::cout << ' ' << 3.14 << std::endl;
+	return 0;
+}
+// std::cout과 << 연산자를 이용한 출력
+// 출력 형태: std::cout << '출력대상';
+
+// << 연산자를 이용한 출력대상의 연이은 표현과 개행
+// std::cout << '출력대상1' << '출력대상2' << '출력대상3';
