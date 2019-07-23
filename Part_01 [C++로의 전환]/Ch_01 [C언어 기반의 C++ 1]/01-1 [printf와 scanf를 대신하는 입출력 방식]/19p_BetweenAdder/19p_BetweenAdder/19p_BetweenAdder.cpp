276363d6a9627e1041b74566c530071e9a5cdfbe
techofficer20 <48676268+techofficer20@users.noreply.github.com>
2019-07-23 15:26:14 +0900

    C++의 지역변수 선언

---
+++ Part_01 [C++로의 전환]/Ch_01 [C언어 기반의 C++ 1]/01-1 [printf와 scanf를 대신하는 입출력 방식]/19p_BetweenAdder/19p_BetweenAdder/19p_BetweenAdder.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+int main(void)
+{
+	int val1, val2;
+	int result = 0;
+	std::cout << "두 개의 숫자입력: ";
+	std::cin >> val1 >> val2;
+
+	if (val1 < val2)
+	{
+		for (int i = val1 + 1; i < val2; i++) // c++의 모든 컴파일러는 지역변수의 선언 위치에 제한을 두지 않는다.
+		{
+			result += i;
+		}
+	}
+	else
+	{
+		for (int i = val2 + 1; i < val1; i++)
+		{
+			result += i;
+		}
+	}
+	std::cout << "두 수 사이의 정수 합: " << result << std::endl;
+	return 0;
+}
