5ea676f9170e1754287956e0fb2231a4c119b407
techofficer20 <48676268+techofficer20@users.noreply.github.com>
2019-07-23 16:38:22 +0900

    부분적 디폴트 값 설정

---
+++ Part_01 [C++로의 전환]/Ch_01 [C언어 기반의 C++ 1]/01-3 [매개변수의 디폴트 값 (Default Value)]/31p_DefaultValue3/31p_DefaultValue3/31p_DefaultValue3.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+int BoxVolume(int length, int width = 1, int height = 1); 
+/*
+부분적으로 디폴트 값을 설정 할 수 있다.
+그런데, (int length = 1, int width, int height) 과 같이는 설정할 수 없다.
+함수에 전달되는 인자가 왼쪽에서부터 오른쪽으로 채워지기 때문이다.
+*/
+int main(void)
+{
+	std::cout << "[3, 3, 3] : " << BoxVolume(3, 3, 3) << std::endl; // 27
+	std::cout << "[5, 5, 0] : " << BoxVolume(5, 5) << std::endl; // 25
+	std::cout << "[7, 0, 0] : " << BoxVolume(7) << std::endl; // 7
+	// std::cout << "[0, 0, 0] : " << BoxVolume() << std::endl; 모든 매개변수에 디폴트 값이 지정된 것이 아니기 때문에 컴파일 에러
+	return 0;
+}
+int BoxVolume(int length, int width, int height)
+{
+	return length * width * height;
+}
