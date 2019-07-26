8edd24318d60d374b970a578afcb019f76edbdc9
techofficer20 <48676268+techofficer20@users.noreply.github.com>
2019-07-27 00:59:17 +0900

    이름공간 기반의 함수 선언과 정의의 구분

---
+++ Part_01 [C++로의 전환]/Ch_01 [C언어 기반의 C++ 1]/01-5 [이름공간(namespace)에 대한 소개]/40p_NameSp2/40p_NameSp2/40p_NameSp2.cpp
@@ -0,0 +1,30 @@
+﻿// 함수의 선언은 헤더파일에, 함수의 정의는 소스파일에 저장하도록..
+
+#include <iostream>
+
+namespace BestComImpl
+{
+	void SimpleFunc(void);
+}
+
+namespace ProgComImpl
+{
+	void SimpleFunc(void);
+}
+
+int main(void)
+{
+	BestComImpl::SimpleFunc();
+	ProgComImpl::SimpleFunc();
+	return 0;
+}
+
+void BestComImpl::SimpleFunc(void)
+{
+	std::cout << "BestCom이 정의한 함수" << std::endl;
+}
+
+void ProgComImpl::SimpleFunc(void)
+{
+	std::cout << "ProgCom이 정의한 함수" << std::endl;
+}
