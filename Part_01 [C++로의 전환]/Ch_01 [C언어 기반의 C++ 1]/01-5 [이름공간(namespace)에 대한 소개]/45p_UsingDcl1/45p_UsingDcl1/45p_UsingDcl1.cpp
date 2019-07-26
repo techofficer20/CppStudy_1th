512de57df75be631c12b83697857ea29a2ffa189
techofficer20 <48676268+techofficer20@users.noreply.github.com>
2019-07-27 02:55:47 +0900

    using을 이용한 이름공간의 명시

---
+++ Part_01 [C++로의 전환]/Ch_01 [C언어 기반의 C++ 1]/01-5 [이름공간(namespace)에 대한 소개]/45p_UsingDcl1/45p_UsingDcl1/45p_UsingDcl1.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+namespace Hybrid
+{
+	void HybFunc(void)
+	{
+		std::cout << "So simple function!" << std::endl;
+		std::cout << "In namespace Hybrid!" << std::endl;
+	}
+}
+
+int main(void)
+{
+	using Hybrid::HybFunc; 
+	// 너무 빈번한 using namespace의 선언은 이름의 충돌을 막기 위한 이름공간의 선언을 의미 없게 만든다. 제한 필요.
+	HybFunc();
+	return 0;
+}
