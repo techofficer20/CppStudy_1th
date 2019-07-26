370b4b7b0bc72452744796699cf01d711db39e97
techofficer20 <48676268+techofficer20@users.noreply.github.com>
2019-07-27 03:03:13 +0900

    using을 이용한 이름공간의 명시 (3)

---
+++ Part_01 [C++로의 전환]/Ch_01 [C언어 기반의 C++ 1]/01-5 [이름공간(namespace)에 대한 소개]/UsingDcl3/UsingDcl3/UsingDcl3.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+/*
+이름공간 std에 선언된 것은 std라는 이름공간의 선언없이 접근하겠다는 선언
+이름공간 std에 선언된 모든 것에 대해 이름공간 지정의 생략을 명령할 수 있다.
+너무 빈번한 using namespace의 선언은 이름의 충돌을 막기 위한 이름공간의 선언을 의미 없게 만든다. 제한 필요
+*/
+int main(void)
+{
+	int num = 20;
+	cout << "Hello World!" << endl;
+	cout << "Hello " << "World!" << endl;
+	cout << num << ' ' << 'A';
+	cout << ' ' << 3.14 << endl;
+	return 0;
+}
