9c89e225d7e671501ff81a83426d126ae9a4709f
techofficer20 <48676268+techofficer20@users.noreply.github.com>
2019-07-27 02:59:48 +0900

    using을 이용한 이름공간의 명시 (2)

---
+++ Part_01 [C++로의 전환]/Ch_01 [C언어 기반의 C++ 1]/01-5 [이름공간(namespace)에 대한 소개]/46p_UsingDcl2/46p_UsingDcl2/46p_UsingDcl2.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+// 이후부터 cin, cout, endl은 std::cin, std::cout, std::endl을 의미한다는 선언
+
+int main(void)
+{
+	int num = 20;
+	cout << "Hello World!" << endl;
+	cout << "Hello " << "World!" << endl;
+	cout << num << ' ' << 'A';
+	cout << ' ' << 3.14 << endl;
+	return 0;
+}
