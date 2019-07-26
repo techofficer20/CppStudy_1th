0e771ac6f781aa54c20d5063d6c605912a8e9a3e
techofficer20 <48676268+techofficer20@users.noreply.github.com>
2019-07-27 03:21:56 +0900

    범위지정 연산자(Scope Resolution Operator)의 또 다른 기능

---
+++ Part_01 [C++로의 전환]/Ch_01 [C언어 기반의 C++ 1]/01-5 [이름공간(namespace)에 대한 소개]/49p_SRO/49p_SRO/49p_SRO.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+int val = 100; // 전역 변수
+
+int SimpleFunc(void)
+{
+	int val = 20; // 지역 변수
+	val += 3; // 지역 변수 3 증가
+	::val += 7; // 전역 변수 7 증가
+	return val;
+}
+
+int main(void)
+{
+	std::cout << SimpleFunc() << std::endl; // 23
+	std::cout << val << std::endl; // 107
+	return 0;
+}
