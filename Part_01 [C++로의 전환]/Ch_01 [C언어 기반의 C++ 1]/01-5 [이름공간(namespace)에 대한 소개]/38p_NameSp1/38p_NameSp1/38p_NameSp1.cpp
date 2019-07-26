9d05736c6ef608d0eb015797dc4cf86be2db35e8
techofficer20 <48676268+techofficer20@users.noreply.github.com>
2019-07-27 00:51:09 +0900

    이름공간의 기본원리

---
+++ Part_01 [C++로의 전환]/Ch_01 [C언어 기반의 C++ 1]/01-5 [이름공간(namespace)에 대한 소개]/38p_NameSp1/38p_NameSp1/38p_NameSp1.cpp
@@ -0,0 +1,29 @@
+﻿/*
+프로젝트의 진행에 있어서 발생할 수 있는 이름의 충돌을 막을 목적으로 존재하는 것이 이름공간이다.
+존재하는 이름공간이 다르면 동일한 이름의 함수 및 변수를 선언하는 것이 가능하다.
+*/
+#include <iostream>
+
+namespace BestComImpl
+{
+	void SimpleFunc(void)
+	{
+		std::cout << "BestCom이 정의한 함수" << std::endl;
+	}
+}
+
+namespace ProgComImpl
+{
+	void SimpleFunc(void)
+	{
+		std::cout << "ProgCom이 정의한 함수" << std::endl;
+	}
+}
+
+int main(void)
+{
+	BestComImpl::SimpleFunc(); // BestComImpl 공간에 있는 SimpleFunc 함수를 호출하겠다
+	ProgComImpl::SimpleFunc(); // ProgComImpl 공간에 있는 SimpleFunc 함수를 호출하겠다
+	// ::는 "범위지정 연산자"라 하며, 이름공간을 지정할 때 사용하는 연산자이다.
+	return 0;
+}
