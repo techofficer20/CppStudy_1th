39f5d846f8d9088d818e400777e664ad7122e80f
techofficer20 <48676268+techofficer20@users.noreply.github.com>
2019-07-27 02:39:03 +0900

    이름공간의 중첩

---
+++ Part_01 [C++로의 전환]/Ch_01 [C언어 기반의 C++ 1]/01-5 [이름공간(namespace)에 대한 소개]/42p_NameSpOverlap/42p_NameSpOverlap/42p_NameSpOverlap.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+namespace Parent
+{
+	int num = 2;
+	
+	namespace SubOne // 이름공간은 중첩이 가능하다. 따라서 계층적 구조를 갖게끔 이름공간을 구성할 수 있다.
+	{
+		int num = 1;
+	}
+
+	namespace SubTwo
+	{
+		int num = 4;
+	}
+}
+int main(void)
+{
+	std::cout << Parent::num << std::endl;
+	std::cout << Parent::SubOne::num << std::endl;
+	std::cout << Parent::SubTwo::num << std::endl;
+}
