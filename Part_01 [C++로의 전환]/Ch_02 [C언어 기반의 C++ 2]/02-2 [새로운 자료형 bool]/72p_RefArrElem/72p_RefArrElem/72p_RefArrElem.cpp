d7dbe4139ca8bc53f11317bf86d12fab47586256
techofficer20 <48676268+techofficer20@users.noreply.github.com>
2019-07-28 20:35:31 +0900

    참조자의 선언 가능 범위

---
+++ Part_01 [C++로의 전환]/Ch_02 [C언어 기반의 C++ 2]/02-2 [새로운 자료형 bool]/72p_RefArrElem/72p_RefArrElem/72p_RefArrElem.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+/*
+참조자의 선언 가능 범위
+상수를 대상으로 참조자를 선언할 수는 없다.
+미리 참조자를 선언했다가, 후에 누군가를 참조하는 것은 불가능하다. 참조의 대상을 바꾸는 것도 불가능하다.
+참조자를 선언하면서 NULL로 초기화하는 것도 불가능하다.
+*/
+int main(void)
+{
+	int arr[3] = { 1, 3, 5 };
+	int& ref1 = arr[0];
+	int& ref2 = arr[1];
+	int& ref3 = arr[2];
+
+	cout << ref1 << endl;
+	cout << ref2 << endl;
+	cout << ref3 << endl;
+
+	return 0;
+}
