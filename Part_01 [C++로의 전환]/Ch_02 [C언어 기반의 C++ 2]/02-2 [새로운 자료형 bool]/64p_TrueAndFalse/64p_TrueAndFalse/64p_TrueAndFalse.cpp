01ff420acfa90def553afdbe9633a506689519b4
techofficer20 <48676268+techofficer20@users.noreply.github.com>
2019-07-28 20:11:30 +0900

    '참'을 의미하는 true와 '거짓'을 의미하는 false

---
+++ Part_01 [C++로의 전환]/Ch_02 [C언어 기반의 C++ 2]/02-2 [새로운 자료형 bool]/64p_TrueAndFalse/64p_TrueAndFalse/64p_TrueAndFalse.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	int num = 10;
+	int i = 0;
+
+	cout << "true: " << true << endl; // 1
+	cout << "false: " << false << endl; // 0
+	// 참과 거짓을 위한 1바이트 크기의 데이터일 뿐, 실제 1과 0은 아니다. 굳이 숫자에 연결시키려 하지 말자.
+
+	while (true)
+	{
+		cout << i++ << ' ';
+		if (i > num)
+			break;
+	}
+		cout << endl;
+
+		cout << "sizeof 1: " << sizeof(1) << endl;
+		cout << "sizeof 0: " << sizeof(0) << endl;
+		cout << "sizeof true: " << sizeof(true) << endl;
+		cout << "sizeof false: " << sizeof(false) << endl;
+		return 0;
+}
