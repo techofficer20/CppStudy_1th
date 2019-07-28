fd7880061af000da49318861744e58082feed3af
techofficer20 <48676268+techofficer20@users.noreply.github.com>
2019-07-28 20:28:21 +0900

    참조자(Reference)의 이해

---
+++ Part_01 [C++로의 전환]/Ch_02 [C언어 기반의 C++ 2]/02-2 [새로운 자료형 bool]/69p_Referen/69p_Referen/69p_Referen.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	int num1 = 1020;
+	int& num2 = num1; // 선언부에 &를 붙이면 주소값을 의미하는 게 아니라 '참조자'를 의미함.
+
+	num2 = 3047;
+
+	cout << "VAL: " << num1 << endl; 
+	cout << "REF: " << num2 << endl;
+
+	cout << "VAL: " << &num1 << endl; // 선언된 이후 &를 붙이면 주소값 의미
+	cout << "REF: " << &num2 << endl;
+
+	return 0;
+}
