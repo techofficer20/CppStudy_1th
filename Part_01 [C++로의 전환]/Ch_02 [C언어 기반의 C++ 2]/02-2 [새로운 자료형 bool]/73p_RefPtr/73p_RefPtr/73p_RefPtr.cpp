34c7ba51493c810882be93d3d7c0c52ee3c19a51
techofficer20 <48676268+techofficer20@users.noreply.github.com>
2019-07-28 20:43:20 +0900

    참조자의 선언 가능 범위 (2)

---
+++ Part_01 [C++로의 전환]/Ch_02 [C언어 기반의 C++ 2]/02-2 [새로운 자료형 bool]/73p_RefPtr/73p_RefPtr/73p_RefPtr.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	int num = 12;
+	int* ptr = &num; // num을 가리키는 포인터 변수 ptr
+	int** dptr = &ptr; // 포인터 ptr을 가리키는 더블 포인터 변수 dptr
+
+	int& ref = num; // num의 참조자 ref
+	int* (&pref) = ptr; // 포인터 ptr의 참조자 포인터 pref
+	int** (&dpref) = dptr; // 더블 포인터 dptr의 참조자 더블 포인터 dpref
+
+	cout << ref << endl;
+	cout << *pref << endl;
+	cout << **dpref << endl;
+
+	// 포인터의 경우에는 참조 대상의 포인터 기호 붙일 것
+	return 0;
+}
