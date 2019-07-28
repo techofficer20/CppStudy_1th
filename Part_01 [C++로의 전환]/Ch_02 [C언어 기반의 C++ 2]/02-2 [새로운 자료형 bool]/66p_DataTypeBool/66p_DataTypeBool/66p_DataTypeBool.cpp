0ac0ec798f9b06a9fd197f2a91e19b8671362ed4
techofficer20 <48676268+techofficer20@users.noreply.github.com>
2019-07-28 20:17:18 +0900

    자료형 bool

---
+++ Part_01 [C++로의 전환]/Ch_02 [C언어 기반의 C++ 2]/02-2 [새로운 자료형 bool]/66p_DataTypeBool/66p_DataTypeBool/66p_DataTypeBool.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+bool IsPositive(int num)
+{
+	if (num <= 0) 
+		return false;
+	else
+		return true;
+
+	// true와 false를 가리켜 bool형 데이터라 한다. 그리고 bool은  int, double과 마찬가지로 기본자료형이다.
+}
+
+int main(void)
+{
+	bool isPos;
+	int num;
+	cout << "Input number: ";
+	cin >> num;
+
+	isPos = IsPositive(num); // isPos에 IsPositive(num) 리턴값을 저장
+	if (isPos) // true로 반환받았을 경우
+		cout << "Positive number" << endl;
+	else // false로 반환받았을 경우
+		cout << "Negative number" << endl;
+
+	return 0;
+}
