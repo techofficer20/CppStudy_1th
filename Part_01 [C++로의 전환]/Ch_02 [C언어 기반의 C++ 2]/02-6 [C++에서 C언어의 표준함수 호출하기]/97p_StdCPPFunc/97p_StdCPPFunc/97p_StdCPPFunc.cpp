3fe4be20ee701d19f1ee26e1c09b5833a99922bd
techofficer20 <48676268+techofficer20@users.noreply.github.com>
2019-07-31 20:27:45 +0900

    c를 더하고 .h를 빼라.

---
+++ Part_01 [C++로의 전환]/Ch_02 [C언어 기반의 C++ 2]/02-6 [C++에서 C언어의 표준함수 호출하기]/97p_StdCPPFunc/97p_StdCPPFunc/97p_StdCPPFunc.cpp
@@ -0,0 +1,17 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <cmath> // math.h
+#include <cstdio> // stdio.h
+#include <cstring> // string.h
+using namespace std; // 모든 표준함수들이 이름공간 std 안에 선언되어 있다.
+
+int main(void)
+{
+	char str1[] = "Result";
+	char str2[30];
+
+	strcpy(str2, str1);
+	printf("%s: %f \n", str1, sin(0.14));
+	printf("%s: %f \n", str2, abs(-1.25));
+	
+	return 0;
+}
