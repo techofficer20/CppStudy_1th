c524cec9f6ccdf572b1bbe21198d864cd671e566
techofficer20 <48676268+techofficer20@users.noreply.github.com>
2019-07-31 18:04:48 +0900

    new & delete

---
+++ Part_01 [C++로의 전환]/Ch_02 [C언어 기반의 C++ 2]/02-5 [malloc & free를 대신하는 new & delete]/91p_MemMalFree/91p_MemMalFree/91p_MemMalFree.cpp
@@ -0,0 +1,19 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string.h>
+#include <stdlib.h>
+using namespace std;
+
+char* MakeStrAdr(int len)
+{
+	char* str = (char*)malloc(sizeof(char) * len); // 문자열 저장을 위한 배열을 힙 영역에 할당
+	return str;
+}
+int main(void)
+{
+	char* str = MakeStrAdr(20);
+	strcpy(str, "I am so happy~");
+	cout << str << endl;
+	free(str); // 힙에 할당된 메모리 공간을 소멸
+	return 0;
+}
