9e08d3bb8a6aa5cd8fe122b405edf3c405222af0
techofficer20 <48676268+techofficer20@users.noreply.github.com>
2019-08-12 21:16:36 +0900

    콘택600의 구현을 통한 캡슐화의 정확한 이해

---
+++ Part_02 [객체지향의 도입]/Ch_04 [클래스의 완성]/163p_Encaps2/163p_Encaps2/163p_Encaps2.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+using namespace std;
+
+class SinivelCap // 콧물 처치용 캡슐
+{
+public:
+	void Take() const { cout << "콧물이 싹~ 납니다." << endl; }
+};
+
+class SneezeCap // 재채기 처치용 캡슐
+{
+public:
+	void Take() const { cout << "재채기가 멎습니다." << endl; }
+};
+
+class SnuffleCap // 코막힘 처치용 캡슐
+{
+public:
+	void Take() const { cout << "코가 뻥 뚫립니다." << endl; }
+};
+
+class CONTAC600
+{
+private:
+	SinivelCap sin;
+	SneezeCap sne;
+	SnuffleCap snu;
+
+public:
+	void Take() const // 캡슐화 (약 복용 순서 바뀌면 여기서 바꾸면 됨)
+	{
+		sin.Take();
+		sne.Take();
+		snu.Take();
+	}
+};
+
+class ColdPatient
+{
+public:
+	void TakeCONTAC600(const CONTAC600& cap) const { cap.Take(); }
+};
+
+int main(void)
+{
+	CONTAC600 cap;
+	ColdPatient sufferer;
+	sufferer.TakeCONTAC600(cap);
+	return 0;
+}
